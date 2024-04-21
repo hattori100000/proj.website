8374454f9b64ebd534eb438c9eabd432e735ba7b
sakar <104910918+hattori100000@users.noreply.github.com>
2024-04-21 22:36:43 +0545

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,33 +1,22 @@
-const express = require("express")
-const router = express.Router()
-const User = require("../model/user")
-const passport = require("passport")
-const { saveRedirectUrl, isLoggedIn } = require("../middleware")
-const wrapAsyc= require("../utils/wrapAsyc")
-const userController = require("../controllers/users")
-const user = require("../model/user")
-
+const express = require("express");
+const router = express.Router();
+const User = require("../model/user");
+const passport = require("passport");
+const { saveRedirectUrl, isLoggedIn } = require("../middleware");
+const wrapAsyc = require("../utils/wrapAsyc");
+const userController = require("../controllers/users");
 
 router.route("/signup")
-.get( userController.rendersignup)
-.post(wrapAsyc(userController.signup)
-)
-//rendersignup
-
-//signup
+  .get(userController.rendersignup)
+  .post(wrapAsyc(userController.signup));
 
 router.route("/login")
-.get(userController.renderlogin)
-.post(saveRedirectUrl,passport.authenticate("local",{failureRedirect:"/login", failureFlash:true}), userController.login)
+  .get(userController.renderlogin)
+  .post(saveRedirectUrl, passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }), userController.login);
 
-//renderlogin form 
+router.get("/logout", userController.logout);
 
-
-//login
-
-
-//logout
-router.get("/logout", userController.logout)
+module.exports = router;
 
 
 
