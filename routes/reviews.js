19c8d122460754d47467f5c4bfd96c196c1e1f7a
sakar <104910918+hattori100000@users.noreply.github.com>
2024-04-21 22:35:54 +0545

    Update reviews.js

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,32 +1,27 @@
-const express = require("express")
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const wrapAsyc = require("../utils/wrapAsyc");
+const ExpressError = require("../utils/ExpressError");
+const { reviewSchema } = require("../schema.js");
+const Review = require("../model/review.js");
+const { isLoggedIn, isReviewAuthor } = require("../middleware.js");
+const reviewController = require("../controllers/reviews.js");
 
-const router = express.Router({mergeParams:true})
-const wrapAsyc= require("../utils/wrapAsyc")
-const ExpressError= require("../utils/ExpressError")
-const {listingSchema, reviewSchema,} = require("../schema.js")
-const Listing = require("../model/listing")
+// Validating review schema
+const validateReviews = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
 
-const Review = require("../model/review.js")
-const { isLoggedIn, isReviewAuthor } = require("../middleware.js")
-const reviewController = require("../controllers/reviews.js")
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
 
-//creatting the schema validatereviews for fnx
-const validatReviews = (req, res, next)=> {
-  let {error} = reviewSchema.validate(req.body)
- 
-if(error){
-  let errMsg =error.details.map((el) => el.message).join(",")
-  throw new ExpressError(400, errMsg)
-}else{
-  next()
-}
-}
+// Routes
+router.post("/", isLoggedIn, validateReviews, wrapAsyc(reviewController.createReview));
 
-//REVIEW POST ROUTE
-router.post("/",isLoggedIn, validatReviews , wrapAsyc(reviewController.createReview))
- 
- //DELETE REVIEW ROUTE
- router.delete("/:reviewId",isLoggedIn, isReviewAuthor,wrapAsyc(reviewController.destroyReview))
- 
- 
-module.exports= router
+router.delete("/:reviewId", isLoggedIn, isReviewAuthor, wrapAsyc(reviewController.destroyReview));
+
+module.exports = router;
