12d8975fcf5722f6ba0f77a3a9fec4fb5a2710e4
sakar <104910918+hattori100000@users.noreply.github.com>
2024-04-21 22:29:55 +0545

    Update reviews.js

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,24 +1,36 @@
-const Review= require("../model/review")
-const Listing = require("../model/listing")
+const Review = require("../model/review");
+const Listing = require("../model/listing");
 
-module.exports.createReview= async(req,res) => {
-  let listing = await Listing.findById(req.params.id)
-  let newReview = new Review(req.body.review)
- newReview.author=req.user._id
-  listing.reviews.push(newReview)
- 
-  await newReview.save()
-  await listing.save()
-  req.flash("success", "New Review created!")
- 
- res.redirect(`/listings/${listing._id}`)
-}
+module.exports.createReview = async (req, res) => {
+  try {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+    newReview.author = req.user._id;
+    listing.reviews.push(newReview);
 
-module.exports.destroyReview= async(req,res)=>{
-  let{ id,reviewId} = req.params
+    await newReview.save();
+    await listing.save();
+    req.flash("success", "New Review created!");
 
-  await Listing.findByIdAndUpdate(id, {$pull:{reviews:reviewId}})
-  await Review.findByIdAndDelete(reviewId)
-  req.flash("success", "New Review deleted!")
-  res.redirect(`/listings/${id}`)
-}
+    res.redirect(`/listings/${listing._id}`);
+  } catch (error) {
+    console.error(error);
+    req.flash("error", "An error occurred while creating the review");
+    res.redirect("back");
+  }
+};
+
+module.exports.destroyReview = async (req, res) => {
+  try {
+    let { id, reviewId } = req.params;
+
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    req.flash("success", "Review deleted!");
+    res.redirect(`/listings/${id}`);
+  } catch (error) {
+    console.error(error);
+    req.flash("error", "An error occurred while deleting the review");
+    res.redirect("back");
+  }
+};
